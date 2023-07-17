b33d80c57fa78477e7669e2a999b6b6bbbbbfbff
Oscar Rodrgues <oscar.rodrigues.dev@gmail.com>
2023-07-16 23:08:52 -0300

    tentativa 4

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ function App() {
       <Routes>
        
         <Route path="CashTarefas/" element={<Admin />} />
-        <Route path="/Tarefeiro/" element={<Tarefeiro />} />
+        <Route path="Tarefeiro/" element={<Tarefeiro />} />
       </Routes>
     </Router>
   );
 }
 /**comentario para tentar subir no git
- * precisa ter mais de uma linha
+
  */
 export default App;
 
